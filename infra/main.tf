ed8e9b934f592b95fb6333befda6f3a88a7893cd
mohammedz00 <zenudeens@gmail.com>
2026-06-11 19:13:48 +0100

    Created: Two subnets per AZ, one public one private

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,5 @@
+
+# VPC
 resource "aws_vpc" "gatus-vpc" {
 
     cidr_block = "10.0.0.0/16"
@@ -7,6 +9,57 @@ resource "aws_vpc" "gatus-vpc" {
 
     tags = {
       Name = "gatus-vpc"
+    }
+  
+}
+
+# Subnets
+resource "aws_subnet" "public_subnet_a" {
+
+    vpc_id = aws_vpc.gatus-vpc
+    cidr_block = "10.0.0.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "eu-west-1a"
+
+    tags = {
+      Name = "PublicSubnetA"
+    }
+  
+}
+
+resource "aws_subnet" "private_subnet_a" {
+
+    vpc_id = aws_vpc.gatus-vpc
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "eu-west-1a"
+
+    tags = {
+      Name = "PrivateSubnetA"
+    }
+  
+}
+
+resource "aws_subnet" "public_subnet_b" {
+
+    vpc_id = aws_vpc.gatus-vpc
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "eu-west-1b"
+
+    tags = {
+      Name = "PublicSubnetB"
+    }
+  
+}
+
+resource "aws_subnet" "private_subnet_b" {
+
+    vpc_id = aws_vpc.gatus-vpc
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "eu-west-1b"
+
+    tags = {
+      Name = "PrivateSubnetB"
     }
   
 }
