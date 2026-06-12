a7fc3c165507448c2893ccfea77c4024cbbad0c9
mohammedz00 <zenudeens@gmail.com>
2026-06-12 19:36:28 +0100

    Added ACM certificate and validation

--- infra/main.tf
+++ infra/main.tf
@@ -372,3 +372,35 @@ resource "aws_route53_record" "app-a-record" {
   
 }
 
+resource "aws_route53_record" "cname" {
+  for_each = {
+    for dvo in aws_acm_certificate.app-certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.app.zone_id
+}
+
+# ACM
+resource "aws_acm_certificate" "app-certificate" {
+    domain_name = "app.zenudeen.com"
+    validation_method = "DNS"
+
+    lifecycle {
+    create_before_destroy = true
+  }
+  
+}
+
+resource "aws_acm_certificate_validation" "app-cert-validation" {
+  certificate_arn = aws_acm_certificate.app-certificate.arn
+  validation_record_fqdns = [ for record in aws_route53_record.cname : record.fqdn ]
+}
