9bce31ac49b8469a8878884bde2de97493c8e0ed
mohammedz00 <zenudeens@gmail.com>
2026-06-12 12:35:55 +0100

    Added: ECS task definition and task execution role

--- infra/main.tf
+++ infra/main.tf
@@ -144,3 +144,78 @@ resource "aws_route_table_association" "private-route-association-b" {
 }
 
 
+
+# ECS
+
+# ECS Cluster
+resource "aws_ecs_cluster" "gatus-ecs-cluster" {
+    name = "gatus-ecs-cluster"
+
+    setting {
+      name = "containerInsights"
+      value = "enabled"
+      # Provide CloudWatch metrics and insights
+    }
+  
+}
+
+# ECS Task Execution Role
+
+# Defining who can use the role
+resource "aws_iam_role" "ecs-task-execution-role" {
+    name = "ecs-task-execution-role"
+assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+          # ECS task agent is the only one who can use this role
+        }
+      }
+    ]
+  }) 
+}
+
+# Defining which permissions/tasks can be done with this role
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy" {
+  role = aws_iam_role.ecs-task-execution-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  # The permissions that this role can use
+}
+
+# ECS Task Definition
+resource "aws_ecs_task_definition" "gatus-task-definition" {
+        family = "gatus-task-definition" # Tasl definition name
+        requires_compatibilities = ["FARGATE"] # Uses fargate only
+        network_mode = "awsvpc" # What network method to use
+        cpu = 512 # 512 CPU refers to 0.5 CPU
+        memory = 1024 # 1024 refers to 1GB memory
+        execution_role_arn = aws_iam_role.ecs-task-execution-role.arn # Uses the ECS task execution role for this task
+
+        container_definitions = jsonencode([{
+                name : "gatus-container" # container name
+                image : "145523122776.dkr.ecr.eu-west-1.amazonaws.com/zenudeen/gatus:latest" # image pulled from ECR
+                essential : true # Requirement to have this image
+                portMappings : [
+                    {
+                        containerPort = 8080 
+                        hostPort = 8080
+                        # Application uses port 8080 so these are assigned accordingly
+                        
+                    }
+                ]
+                     
+            }
+        ]
+        )
+        runtime_platform {
+                operating_system_family = "LINUX" 
+                cpu_architecture = "X86_64"
+                # specifying runtime requirements
+            }
+
+  
+}
